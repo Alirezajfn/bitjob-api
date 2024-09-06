8b7baf4691d621ff4054b2adb6590601d9b630b1
Alireza Jafarinejad <alirezajfn1@gmail.com>
2024-09-06 19:59:17 +0330

    Add JWT authentication with login and token refresh endpoints

--- config/settings.py
+++ config/settings.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+from datetime import timedelta
 from pathlib import Path
 
 import environ
@@ -128,3 +129,16 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=60),
+    "USER_ID_FIELD": "username",
+    "USER_ID_CLAIM": "username",
+}
