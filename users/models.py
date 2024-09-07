e87de9501320b93dd401ea691c5ef0f5ced5e1d4
Alireza Jafarinejad <alirezajfn1@gmail.com>
2024-09-07 09:36:37 +0330

    Add custom User model with unique related names for groups and permissions

--- users/models.py
+++ users/models.py
@@ -1,3 +1,34 @@
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from users.managers import CustomUserManager
+
+
+class User(AbstractUser):
+    objects = CustomUserManager()
+    email = models.EmailField(unique=True)
+    REQUIRED_FIELDS = ['email']
+
+    groups = models.ManyToManyField(
+        'auth.Group',
+        verbose_name=_('groups'),
+        blank=True,
+        help_text=_('The groups this user belongs to. A group represents a collection of users who have'
+                    ' certain permissions.'),
+        related_name="user_set_custom",
+        related_query_name="user_custom",
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        verbose_name=_('user permissions'),
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_name="user_perm_set_custom",
+        related_query_name="user_permission_custom",
+    )
+
+    class Meta:
+        indexes = [
+            models.Index(fields=['email', ]),
+        ]
